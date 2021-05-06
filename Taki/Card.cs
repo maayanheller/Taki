5bd354ba09112fd8f29407a66b82ad3c767834ed
maayanheller <maayan.heller@gmail.com>
2021-05-06 20:56:35 +0300

    Added a card class with 2 attribures and Get functions

---
+++ Taki/Card.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taki
+{
+    class Card
+    {
+        private int cardNum;
+        private string cardColor;
+
+        public Card(int cardNum, string cardColor)
+        {
+            this.cardNum = cardNum;
+            this.cardColor = cardColor;
+        }
+
+        public int GetCardNum()
+        {
+            return this.cardNum;
+        }
+
+        public string GetCardColor()
+        {
+            return this.cardColor;
+        }
+    }
+}
